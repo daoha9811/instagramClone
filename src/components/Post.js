3349cebf3c44ecdec1a1de7fc68dd9bb56cf7342
Glitch (sophisticated-repeated-map) <none>
2020-05-21 09:26:07 +0000

    🚘🐉 Updated with Glitch

---
+++ src/components/Post.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Modal, Breadcrumb } from "antd";
+import PostModal from "./PostModal";
+
+export default props => {
+  
+  const {img, id, userName} = props;
+  
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const modalTitle = userName => {
+    return (
+      <Breadcrumb separator="•">
+        <Breadcrumb.Item>
+          <span style={{ fontWeight: "700" }}>{userName}</span>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item href="">
+          <span style={{ color: "#0095f6" }}>Theo Doi</span>
+        </Breadcrumb.Item>
+      </Breadcrumb>
+    );
+  };
+
+  return (
+    <div className="avatar">
+      <img
+        onClick={showModal}
+        height="250"
+        src={img}
+        style={{ cursor: "pointer", width: "100%" }}
+      />
+      <Modal
+        title={modalTitle(userName)}
+        visible={isModalVisible}
+        onCancel={handleCancel}
+        style={{ maxWidth: "70%" }}
+        width="70%"
+        footer={null}
+      >
+        <PostModal postId={id} />
+      </Modal>
+    </div>
+  );
+};
