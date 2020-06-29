fb02a18577e5a43ce6f175f46f21d2e984f118cb
daoha <daoha9811@gmail.com>
2020-06-30 00:57:42 +0700

    fix-giaodien(2)

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -3,7 +3,7 @@ import EmptyImg from '../assets/images/empty.svg';
 
 
 export default () => {
-    return <div>
+    return <div className="home-page">
         <img alt="empty" src={EmptyImg} width="200" height="200" />
         <span>Hiện tại tính năng đang được cập nhập mời sử dụng tính năng khác</span>
     </div>
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NotFoundImg from '../assets/images/notfound.svg';
 
 export default () => {
-    return <div>
+    return <div className="not-found" >
         <img alt="empty" src={NotFoundImg} width="200" height="200" />
         <span>Page not found</span>
     </div>
